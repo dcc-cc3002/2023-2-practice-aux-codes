00aeee581054ae9d30b8aac4dada5199277f7626
danielRamirezL <danielramirez@ug.uchile.cl>
2023-09-01 15:33:11 -0400

    Add aux4 problem

---
+++ src/main/scala/aux4/problem/ICharacter.scala
@@ -0,0 +1,25 @@
+package aux4.problem
+
+class ICharacter(val name: String,
+                 val healthPoints: Int,
+                 val manaPoints: Int,
+                 val characterType: String) {
+
+  if (characterType == "Mercenary" || characterType == "Acrobat") {
+    manaPoints = 0
+  }
+
+  def warCry(): String = {
+    "For the glory!"
+  }
+
+  def regenerateHp(): Unit = {
+    healthPoints += 1
+  }
+
+  def regenerateMp(): Unit = {
+    if (characterType == "Assist" || characterType == "Magician") {
+      manaPoints += 1
+    }
+  }
+}
